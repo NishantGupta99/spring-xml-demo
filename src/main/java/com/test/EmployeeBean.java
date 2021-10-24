fe77156ca084ad97be1d8a8e3e135901b3926f20
nishant Gupta <kr.nish99@gmail.com>
2021-10-24 23:11:48 +0530

    autowiring with contructor

--- src/main/java/com/test/EmployeeBean.java
+++ src/main/java/com/test/EmployeeBean.java
@@ -1,24 +1,33 @@
 package com.test;
 
-public class EmployeeBean
-{
-    private DepartmentBean departmentBean ; //dependency
-    public EmployeeBean(DepartmentBean departmentBean, String fullName) {
-    	this.departmentBean = departmentBean;
-    	this.fullName = fullName;
-    }
-    private String fullName;
- 
-    void applyJob() {
-    	System.out.println(departmentBean);
-    	departmentBean.receiveApplication();
-    }
-    
-    
-    public String getFullName() {
-        return fullName;
-    }
-    public void setFullName(String fullName) {
-        this.fullName = fullName;
-    }
+public class EmployeeBean {
+	private DepartmentBean departmentBean; // dependency
+
+	public EmployeeBean(DepartmentBean departmentBean, String fullName) {
+		this.departmentBean = departmentBean;
+		this.fullName = fullName;
+	}
+
+	private String fullName;
+
+	void applyJob() {
+		System.out.println(departmentBean);
+		departmentBean.receiveApplication();
+	}
+
+	public DepartmentBean getDepartmentBean() {
+		return departmentBean;
+	}
+
+	public void setDepartmentBean(DepartmentBean departmentBean) {
+		this.departmentBean = departmentBean;
+	}
+
+	public String getFullName() {
+		return fullName;
+	}
+
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
 }
